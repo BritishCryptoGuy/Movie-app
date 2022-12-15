c3e5c30c47ee3f30c4d9a32dda5662dc6492b69b
Kieran <108032076+BritishCryptoGuy@users.noreply.github.com>
2022-12-15 20:23:13 +0000

    Finished jquery version

--- js/script.js
+++ js/script.js
@@ -1,58 +1,45 @@
-var searchInput = document.querySelector(".search");
-var cardWrapper = document.querySelector("main");
+var searchInput = $(".search");
+var cardWrapper = $("main");
 
 function noMatch() {
-  cardWrapper.innerHTML = "<p class='no-search'>No results found. </p>";
+  cardWrapper.html("<p class='no-search'>No results found. </p>");
 }
+
 function displayMatches(matches) {
-  cardWrapper.innerHTML = "";
-  if (!matches.length) {
+  cardWrapper.html("");
+  if (!matches) {
     noMatch();
   }
   for (let matchObj of matches) {
     let imdb = matchObj.imdbID;
-    fetch(`https://www.omdbapi.com/?apikey=257d3522&i=${imdb}`)
-      .then((data) => data.json())
-      .then(function (data) {
-        let plot = data.Plot;
-        cardWrapper.insertAdjacentHTML(
-          "beforeend",
-          `<div class='movie-card' style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${matchObj.Poster});"><h3>${matchObj.Title}</h3><p>${plot}</p> <a href='https://imdb.com/title/${matchObj.imdbID}' target="_blank">View More Info Here</a></div>`
-        );
-      });
+    $.get(`https://www.omdbapi.com/?apikey=257d3522&i=${imdb}`).then(function (
+      data
+    ) {
+      let plot = data.Plot;
+      cardWrapper.append(
+        `<div class='movie-card' style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${matchObj.Poster});"><h3>${matchObj.Title}</h3><p>${plot}</p> <a href='https://imdb.com/title/${matchObj.imdbID}' target="_blank">View More Info Here</a></div>`
+      );
+    });
   }
 }
 
 function fetchMovies(event) {
   var keyCode = event.keyCode;
-  var searchText = searchInput.value.toLowerCase().trim();
+  var searchText = searchInput.val().trim();
 
   if (keyCode === 13 && searchText) {
-    let responsePromise = fetch(
-      `https://www.omdbapi.com/?apikey=257d3522&s=${searchText}`
+    console.log(searchText);
+    $.get(`https://www.omdbapi.com/?apikey=257d3522&s=${searchText}`).then(
+      function (data) {
+        displayMatches(data.Search);
+        searchInput.val("");
+      }
     );
-
-    function handleResponse(responseObj) {
-      return responseObj.json();
-    }
-
-    responsePromise.then(handleResponse).then(function (data) {
-      let dataArray = data.Search;
-      displayMatches(dataArray);
-      searchInput.value = "";
-    });
-
-    console.log("test");
-    // responsePromise.then(function (responseObj) {
-    //   let dataPromise = responseObj.json();
-    //   console.log(dataPromise);
-    //   dataPromise.then(function (data) {
-    //     console.log(data);
   }
 }
 
 function init() {
-  searchInput.addEventListener("keydown", fetchMovies);
+  searchInput.keydown(fetchMovies);
 }
 
 init();
